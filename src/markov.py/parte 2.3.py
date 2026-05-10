b85e538c5e74cf73ed6f490e10facd331e75bed1
gabiperez12 <gabiperez041111@gmail.com>
2026-05-10 20:00:43 -0300

    Update parte 2.3.py

--- src/markov.py/parte 2.3.py
+++ src/markov.py/parte 2.3.py
@@ -1,1 +1,15 @@
 
+#2.3
+#a)
+#A partir de la tasa de entropía empírica de orden 4 se estimó el tamaño mínimo teórico al que podrían comprimirse los textos sin pérdida. 
+# Para el texto en inglés, con H^4≈0.256 bits por símbolo y una longitud de 13 079 caracteres, el tamaño mínimo teórico 
+# resulta del orden de 419 bytes. Para el texto en español, con H^4≈0.347 bits por símbolo y 12 040 caracteres, el tamaño mínimo 
+#teórico estimado es de aproximadamente 522 bytes. 
+#Estos valores representan límites ideales de compresión y reflejan una mayor predictibilidad del texto en inglés para órdenes altos del modelo.
+
+
+#b)
+#A partir de la tasa de entropía empírica de orden 4 se estimó el ratio de compresión teórico para ambos textos. Para el inglés, con H^4≈0.256 bits por símbolo, 
+# el ratio de compresión teórico resulta de aproximadamente 31:1, mientras que para el español, con H^4≈0.347 bits por símbolo, el 
+# ratio estimado es de alrededor de 23:1. 
+# Estos valores corresponden a límites ideales de compresión sin pérdida y reflejan una mayor predictibilidad del texto en inglés para órdenes altos del modelo de Markov.
